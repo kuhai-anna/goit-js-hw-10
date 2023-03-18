d7ab67104c3acf7e8d8edd2daa8964d9bbd9301a
KuhaiAnna <kuhai.anna@gmail.com>
2023-03-18 19:24:04 +0200

    Update code for the task

--- src/index.js
+++ src/index.js
@@ -5,6 +5,14 @@ import { fetchCountries } from './js/fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
 
+// Налаштування сповіщень
+const notiflixOptions = {
+  width: '330px',
+  fontSize: '16px',
+  position: 'right-top',
+  timeout: 2000,
+};
+
 // Посилання на елементи
 const refs = {
   inputEl: document.querySelector('#search-box'),
@@ -17,7 +25,7 @@ refs.inputEl.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 
 // Пошук країни
 function onSearch(e) {
-  e.preventDefault();
+  removeMarkup(); // динамічне видалення розмітки
 
   const input = e.target;
   const searchQuery = input.value.trim();
@@ -25,63 +33,59 @@ function onSearch(e) {
   if (searchQuery) {
     fetchCountries(searchQuery).then(renderMarkup).catch(onFetchError);
   } else {
-    // динамічне видалення розмітки
-    refs.countryList.innerHTML = '';
-    refs.countryBox.innerHTML = '';
+    removeMarkup(); // динамічне видалення розмітки
   }
 }
 
 // Створення розмітки
 function renderMarkup(result) {
   if (result.length > 10) {
-    refs.countryList.innerHTML = ''; // динамічне видалення розмітки
-
-    Notify.info('Too many matches found. Please enter a more specific name.');
+    Notify.info(
+      'Too many matches found. Please enter a more specific name.',
+      notiflixOptions
+    );
   } else if (result.length <= 10 && result.length != 1) {
-    refs.countryBox.innerHTML = ''; // динамічне видалення розмітки
-
-    const markupCoutryItem = createCountryItemTpl(result); // створення розмітки
-    refs.countryList.innerHTML = markupCoutryItem; // динамічне додавання розмітки
+    createCountryItemTpl(result); // створення розмітки
   } else {
-    refs.countryList.innerHTML = ''; // динамічне видалення розмітки
-
-    const markupCountryCard = createCountryCardTpl(result); // створення розмітки
-    refs.countryBox.innerHTML = markupCountryCard; // динамічне додавання розмітки
+    createCountryCardTpl(result); // створення розмітки
   }
 }
 
 // Створення списку країн
 function createCountryItemTpl(array) {
-  return array
+  const markupCoutryItem = array
     .map(
       ({ name, flags }) =>
-        `<li class="country-item"><img class="country-flag" width="30" height="24" src="${flags.svg}" alt="${flags.alt}">${name.official}</li>`
+        `<li class="country-item"><img class="country-flag" width="35" height="30" src="${flags.svg}" alt="Flag of ${name.official}"><p>${name.official}</p></li>`
     )
     .join('');
+
+  refs.countryList.innerHTML = markupCoutryItem; // динамічне додавання розмітки
 }
 
 // Створення карточки країни
 function createCountryCardTpl(array) {
   const cardMarkup = array
-    .map(({ name, capital, population, flags, languages }) => {
-      // languages - це обʼєкт, який мість: ключі (коди мов) та їх значення (назви мови)
-      // витягуємо назву кожної мови з обʼєкта
-      const keys = Object.keys(languages);
-      const languageArray = [];
-
-      for (const key of keys) {
-        languageArray.push(`<li class="langueges-item">${languages[key]}</li>`);
-      }
-
-      const languageItems = languageArray.join('');
-
-      return `<h1 class="country-name"><img class="country-flag" width="30" height="24" src="${flags.svg}" alt="${flags.alt}">${name.official}</h1><ul class="description-list list"><li class="description-item"><h2 class="description-title">Capital: </h2><span>${capital}</span></li><li class="description-item"><h2 class="description-title">Population: </h2><span>${population}</span></li><li class="description-item"><h2 class="description-title">Languages: </h2><ul class="language-list">${languageItems}</ul></li></ul>`;
-    })
+    .map(
+      ({ name, capital, population, flags, languages }) =>
+        `<h1 class="country-name"><img class="country-flag" width="40" height="34" src="${
+          flags.svg
+        }" alt="Flag of ${name.official}"><p>${
+          name.official
+        }</p></h1><ul class="description-list list"><li class="description-item"><h2 class="description-title">Capital: </h2><p>${capital}</p></li><li class="description-item"><h2 class="description-title">Population: </h2><p>${population}</p></li><li class="description-item"><h2 class="description-title">Languages: </h2><p class="languages-list">${Object.values(
+          languages
+        ).join(', ')}</p></li></ul>`
+    )
     .join('');
 
-  return cardMarkup;
+  refs.countryBox.innerHTML = cardMarkup; // динамічне додавання розмітки
+}
+
+function removeMarkup() {
+  refs.countryList.innerHTML = '';
+  refs.countryBox.innerHTML = '';
 }
 
 function onFetchError() {
-  Notify.failure('Oops, there is no country with that name');
+  Notify.failure('Oops, there is no country with that name', notiflixOptions);
 }
